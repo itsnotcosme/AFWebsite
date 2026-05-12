1520c2e3b31f5461bd51cf5c58702da0edea0b5d
Alberto Ferreira <beto@MacBookPro.lan>
2026-05-12 18:11:09 +0100

    Remove accidental Next.js build artifacts

--- alberto-website/.next/dev/static/chunks/[project]_Documents_GitHub_AlbertoWebsite_alberto-website_pages__error_07xvfw~._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/13za_next_dist_compiled_0por_bf._.js","static/chunks/13za_next_dist_shared_lib_0~dcmhs._.js","static/chunks/13za_next_dist_client_0r_.n9s._.js","static/chunks/13za_next_dist_0rk00u6._.js","static/chunks/0xnn_next_dist_pages__error_0t0fz22.js","static/chunks/[next]_entry_page-loader_ts_0~za8eo._.js","static/chunks/13za_react-dom_0ziun9p._.js","static/chunks/13za_0u6sfdd._.js","static/chunks/[root-of-the-server]__05jz523._.js"],
-    source: "entry"
-});
